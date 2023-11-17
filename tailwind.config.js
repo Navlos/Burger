b7fd14737e4abb603223793dc8f2d4bdcf9cdc8c
Adetayo <adetunjitayo2019@gmail.com>
2023-11-16 17:59:03 -0800

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.{html,js}"],
+  darkMode: 'class',
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px"
+    },
     extend: {
-      screens :{
-        sm:"480px",
-        md:"768px",
-        lg:"1024px",
-
-      },
-      colors:{
+      colors: {
         primaryColor: '#010a5e',
         primaryColorLight: "#010d78",
         secondaryColor: '#FFCC00',
@@ -20,14 +20,27 @@ module.exports = {
         redColor: "#cc3433",
         darkColor: '#000',
         darkColorLight: "#171717",
-       
       },
-      fontFamily: {
-        oswald: ['Oswald', 'sans-serif'],
-        dmsans: ['DM Sans', 'sans-serif']
+      keyframes: {
+        move: {
+          "50%": {transform: 'translateY(-1rem)'}
+        }
+      },
+      animation: {
+        'movingY': 'move 2s linear infinite'
       }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem'
+      }
+    },
+    fontFamily: {
+      oswald: ['Oswald', 'sans-serif'],
+      dmsans: ['DM Sans', 'sans-serif']
+    }
   },
   plugins: [],
 }
-
